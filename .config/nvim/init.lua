c751060e13f7ec827bc7e367cdffcc8dbaaf14f1
Arch Config Backup <backup@localhost>
2025-07-28 20:20:11 +0000

    Automated Backup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,115 +1,146 @@
--- Leader key
 vim.g.mapleader = ' '
-
--- Essential options
 vim.opt.number, vim.opt.cursorline, vim.opt.signcolumn = true, true, 'yes'
 vim.opt.undofile, vim.opt.ignorecase, vim.opt.smartcase = true, true, true
+vim.opt.termguicolors, vim.opt.splitbelow, vim.opt.splitright = true, true, true
+vim.opt.scrolloff, vim.opt.sidescrolloff, vim.opt.updatetime = 8, 8, 250
 vim.schedule(function() vim.opt.clipboard = 'unnamedplus' end)
 
--- Lazy.nvim bootstrap
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', 'https://github.com/folke/lazy.nvim.git', lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
-
--- Plugin specifications
 require('lazy').setup({
   {
-    'nvim-telescope/telescope.nvim',
-    cmd = 'Telescope',
+    'ibhagwan/fzf-lua',
     keys = { '<leader>sf', '<leader>sg', '<leader>sb', '<leader>sh' },
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', cond = function() return vim.fn.executable('make') == 1 end },
-    },
     config = function()
-      require('telescope').setup()
-      pcall(require('telescope').load_extension, 'fzf')
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>sf', builtin.find_files)
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep)
-      vim.keymap.set('n', '<leader>sb', builtin.buffers)
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags)
+      require('fzf-lua').setup({
+        winopts = {
+          height = 0.85,
+          width = 0.80,
+          row = 0.35,
+          col = 0.50,
+          border = 'rounded',
+        },
+        keymap = {
+          fzf = {
+            ['ctrl-q'] = 'select-all+accept',
+          },
+        },
+        files = {
+          rg_opts = "--color=never --files --hidden --follow -g '!.git'",
+        },
+        grep = {
+          rg_opts = "--column --line-number --no-heading --color=always --smart-case --max-columns=4096",
+        },
+      })
+      
+      local fzf = require('fzf-lua')
+      vim.keymap.set('n', '<leader>sf', fzf.files)
+      vim.keymap.set('n', '<leader>sg', fzf.live_grep)
+      vim.keymap.set('n', '<leader>sb', fzf.buffers)
+      vim.keymap.set('n', '<leader>sh', fzf.help_tags)
     end,
   },
   {
-    'catppuccin/nvim',
-    name = 'catppuccin',
+    'rose-pine/neovim',
+    name = 'rose-pine',
     lazy = false,
     priority = 1000,
-    opts = { flavour = 'mocha' },
-    config = function() vim.cmd.colorscheme('catppuccin') end,
+    config = function()
+      require('rose-pine').setup({
+        variant = 'auto',
+        dark_variant = 'moon',
+        styles = {
+          bold = false,
+          italic = false,
+          transparency = false,
+        },
+      })
+      vim.cmd.colorscheme('rose-pine')
+    end
   },
-  
   {
     'echasnovski/mini.nvim',
     event = 'UIEnter',
     config = function()
-      require('mini.statusline').setup({ use_icons = false })
-      require('mini.indentscope').setup()
-    end,
+      require('mini.statusline').setup({
+        use_icons = false,
+        set_vim_settings = false,
+      })
+      
+      require('mini.indentscope').setup({
+        symbol = '│',
+        options = { try_as_border = true },
+        draw = {
+          delay = 100,
+          animation = require('mini.indentscope').gen_animation.none(),
+        },
+      })
+    end
   },
-  
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     event = { 'BufReadPost', 'BufNewFile' },
     config = function()
       require('nvim-treesitter.configs').setup({
-        ensure_installed = { 'python', 'lua', 'vim' },
-        highlight = { enable = true },
+        ensure_installed = { 
+          'python', 'lua', 'vim', 'vimdoc', 'query',
+          'bash', 'json', 'yaml', 'toml', 'markdown',
+          'typescript', 'javascript'
+        },
+        sync_install = false,
+        auto_install = true,
+        highlight = { 
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
         indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<C-space>',
+            node_incremental = '<C-space>',
+            scope_incremental = false,
+            node_decremental = '<bs>',
+          },
+        },
       })
-    end,
+      
+      vim.opt.foldmethod = 'expr'
+      vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+      vim.opt.foldenable = false
+    end
   },
 })
 
--- Modern LSP configuration (Neovim 0.11+)
 vim.lsp.config('*', {
-  on_attach = function(client, bufnr)
-    local map = function(keys, func) vim.keymap.set('n', keys, func, { buffer = bufnr }) end
-    local builtin = require('telescope.builtin')
-    map('grd', builtin.lsp_definitions)
-    map('grr', builtin.lsp_references)
-    map('gra', vim.lsp.buf.code_action)
-    map('grn', vim.lsp.buf.rename)
-    map('K', vim.lsp.buf.hover)
+  on_attach = function(_, bufnr)
+    local map = function(k, f, desc) 
+      vim.keymap.set('n', k, f, { buffer = bufnr, desc = desc }) 
+    end
+    local fzf = require('fzf-lua')
+    map('gd', vim.lsp.buf.definition, 'Go to definition')
+    map('gr', vim.lsp.buf.references, 'Go to references')  
+    map('gi', vim.lsp.buf.implementation, 'Go to implementation')
+    map('<leader>ca', vim.lsp.buf.code_action, 'Code action')
+    map('<leader>rn', vim.lsp.buf.rename, 'Rename symbol')
+    map('K', vim.lsp.buf.hover, 'Hover documentation')
+    map('<C-k>', vim.lsp.buf.signature_help, 'Signature help')
   end,
 })
 
-vim.lsp.config('ruff', { 
+vim.lsp.config('ruff', {
   cmd = { '/home/seremark/.local/ruff-venv/bin/ruff', 'server' },
   filetypes = { 'python' },
-  on_attach = function(client) client.server_capabilities.hoverProvider = false end 
+  on_attach = function(c) c.server_capabilities.hoverProvider = false end
 })
-vim.lsp.config('basedpyright', { 
+vim.lsp.config('basedpyright', {
   cmd = { '/home/seremark/.local/basedpyright-venv/bin/basedpyright-langserver', '--stdio' },
   filetypes = { 'python' },
   settings = { basedpyright = { analysis = { typeCheckingMode = 'basic', autoSearchPaths = true } } },
-  before_init = function(_, config)
-    local function find_python_path()
-      if vim.env.VIRTUAL_ENV then
-        return vim.env.VIRTUAL_ENV .. '/bin/python'
-      end
-      
-      local cwd = vim.fn.getcwd()
-      local candidates = {
-        cwd .. '/venv/bin/python',
-        cwd .. '/.venv/bin/python',
-        vim.fn.expand('~/') .. 'projects/' .. vim.fn.fnamemodify(cwd, ':t') .. '/venv/bin/python'
-      }
-      
-      for _, path in ipairs(candidates) do
-        if vim.fn.executable(path) == 1 then
-          return path
-        end
-      end
-      
-      return 'python3'
-    end
-    
-    config.settings.python = { pythonPath = find_python_path() }
-  end,
+  before_init = function(_, c) c.settings.python = { pythonPath = vim.fn.getcwd() .. '/venv/bin/python' } end
 })
 vim.lsp.enable({ 'ruff', 'basedpyright' })
